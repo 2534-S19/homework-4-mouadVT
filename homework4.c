6ca93ee84903776529f58647043578779f736cee
mouad ait taleb ali <mouad@vt.edu>
2020-04-09 23:55:59 -0400

    code with comments

--- homework4.c
+++ homework4.c
@@ -5,23 +5,21 @@ int main(void)
 {
     char rChar;
     char *response = "\n\n\r2534 is the best course in the curriculum!\r\n\n";
-    int i = 0;
+    int i = 0; // the symbol to run the string in the while loop
     // TODO: Declare the variables that main uses to interact with your state machine.
     int pass;
-    //bool finished = false;
-/*    bool All_done(char Success_key){
+    char key;
+    bool finished;
+/*    bool All_done(key){
         if( Success_key == '\0')
             finished = false;
-        else
-            finished = true ;
-        return finished;
-    }*/
+ */
     // Stops the Watchdog timer.
     initBoard();
     // TODO: Declare a UART config struct as defined in uart.h.
 
     //       To begin, configure the UART for 9600 baud, 8-bit payload (LSB first), no parity, 1 stop bit.
-    eUSCI_UART_ConfigV1 uartConfig = {
+    eUSCI_UART_ConfigV1 theConfig = {
             EUSCI_A_UART_CLOCKSOURCE_SMCLK, 19, 8, 0x55,
             EUSCI_A_UART_NO_PARITY,
             EUSCI_A_UART_LSB_FIRST,
@@ -36,7 +34,7 @@ int main(void)
 
     // TODO: Initialize EUSCI_A0
 
-    UART_initModule(EUSCI_A0_BASE, &uartConfig);
+    UART_initModule(EUSCI_A0_BASE, &theConfig);
 
     // TODO: Enable EUSCI_A0
 
@@ -47,7 +45,7 @@ int main(void)
         // TODO: Check the receive interrupt flag to see if a received character is available.
         //       Return 0xFF if no character is available.
         if (UART_getInterruptStatus(EUSCI_A0_BASE,EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG) == EUSCI_A_UART_RECEIVE_INTERRUPT_FLAG)
-            rChar = UART_receiveData(EUSCI_A0_BASE);
+            rChar = UART_receiveData(EUSCI_A0_BASE); // here the char get from mobaXterm to LaunchPad
         else
             rChar = 0xFF;
 
@@ -58,58 +56,60 @@ int main(void)
         {
           if (UART_getInterruptStatus(EUSCI_A0_BASE,EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) == EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG)
             {
-                UART_transmitData(EUSCI_A0_BASE, rChar);
+                UART_transmitData(EUSCI_A0_BASE, rChar);// here the char get from LaunchPad back to mobaXterm to display
             }
         }
-        if ((rChar == '2'))
-        {
-            charFSM(rChar);
-        }
-        if ((rChar == '5'))
-        {
-            charFSM(rChar);
-        }
-        if ((rChar == '3'))
-        {
-            charFSM(rChar);
-        }
-        if ((rChar == '4'))
-        {
-            charFSM(rChar);
-        }
-        if (charFSM(rChar)|| (rChar == '4') )
-        {
-            pass = 1;
-        }
-        else
-        {
-            pass = 0;
-        }
-
-
-        // TODO: If the FSM indicates a successful string entry, transmit the response string.
-        //       Check the transmit interrupt flag prior to transmitting each character and moving on to the next one.
-        //       Make sure to reset the success variable after transmission.
-
-
-           if (pass == 1)
-           {
-            while (response[i] != '\0')
-            {
-                if (UART_getInterruptStatus(EUSCI_A0_BASE,EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) == EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG)
+                if ((rChar == '2'))
                 {
-                    UART_transmitData(EUSCI_A0_BASE, response[i]);
-                    i++;
-                  // char Success_key = response[i];
+                    charFSM(rChar);
+                }
+                if ((rChar == '5'))
+                {
+                    charFSM(rChar);
+                }
+                if ((rChar == '3'))
+                {
+                    charFSM(rChar);
+                }
+                if ((rChar == '4'))
+                {
+                    charFSM(rChar);
+                }
+                if (charFSM(rChar)|| (rChar == '4') )
+                {
+                    pass = 1;
+                }
+                else
+                {
+                    pass = 0;
                 }
 
-            }
+
+                // TODO: If the FSM indicates a successful string entry, transmit the response string.
+                //       Check the transmit interrupt flag prior to transmitting each character and moving on to the next one.
+                //       Make sure to reset the success variable after transmission.
+
+
+                   if (pass == 1) // if condition satisfied the while loop start acting
+                   {
+                    while (response[i] != '\0')
+                    {
+                        if (UART_getInterruptStatus(EUSCI_A0_BASE,EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG) == EUSCI_A_UART_TRANSMIT_INTERRUPT_FLAG)
+                        {
+                            UART_transmitData(EUSCI_A0_BASE, response[i]); // each element on the string pushed to MobaXterm and displyed
+                            i++;
+                            key = response[i]; //success variable after transmission
+                        }
+                                if( key == '\0')
+                                 finished = false;
+                    }
+
+                    }
+
 
             }
+        }
 
-
-    }
-}
 
 void initBoard()
 {
@@ -122,7 +122,7 @@ bool charFSM(char rChar)
 {
     bool finished ;
 
-    typedef enum {S2, S25, S253, S2534} All_states;
+    typedef enum {S2, S25, S253, S2534} All_states; // there are 4 states
     static All_states state = S2;
 
     switch (state)
@@ -180,7 +180,7 @@ bool charFSM(char rChar)
         if (rChar == '4')
         {
             state = S2;
-            finished = true;
+            finished = true; // the state where the FSM returns the true value
         }
         else if (rChar == '2')
         {
